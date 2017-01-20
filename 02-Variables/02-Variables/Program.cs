4a412503a74edd564725de114273ecdc6b2cdc8c
sky609 <vinex609@gmail.com>
2017-01-21 00:58:32 +0100

    Variables
    This is Variables Lesson in C#

---
+++ 02-Variables/02-Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("My Variable Value is = {0}",x);
+            Console.ReadLine();
+
+            string str = ("'This is a Text'");
+            Console.WriteLine("\nThe Integer is = {0} \nUnd The String is ={1}", x, str);
+            Console.ReadLine();
+        }
+    }
+}
